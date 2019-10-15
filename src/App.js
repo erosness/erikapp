b17e8cc6d6322ae7a855abd3e576ac8626f6fb80
Erik Rosness <erik.rosness@tandbergradio.com>
2019-10-15 15:12:45 +0200

    Complete mini-test-app.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+function callout() {
+  const string = "Restaurants in Hanalei in operation";
+  const urlFriendly = string => string.split(" ");
+  const reversex = array => array.reverse();
+  const connect = array => {
+    if (array.length === 1) {
+      return array[0];
+    } else {
+      return (array[0] + "-" + connect(array.slice(1)));
+    }
+  }
+
+
+  console.log(connect(reversex(reversex(urlFriendly(string))))); // "Restaurants-in-Hanalei"
+  console.log(string);
+
+}
+
+const headerLine = React.createElement("h1", { id: "recipe-0" }, "Baked Salmon");
+console.log(headerLine);
+
+const data = [
+  {
+    name: "Baked Salmon",
+    ingredients: [
+      { name: "Salmon", amount: 1, measurement: "l lb" },
+      { name: "Pine Nuts", amount: 1, measurement: "cup" },
+      { name: "Butter Lettuce", amount: 2, measurement: "cups" },
+      { name: "Yellow Squash", amount: 1, measurement: "med" },
+      { name: "Olive Oil", amount: 0.5, measurement: "cup" },
+      { name: "Garlic", amount: 3, measurement: "cloves" }
+    ],
+    steps: [
+      "Preheat the oven to 350 degrees.",
+      "Spread the olive oil around a glass baking dish.",
+      "Add the yellow squash and place in the oven for 30 mins.",
+      "Add the salmon, garlic, and pine nuts to the dish.",
+      "Bake for 15 minutes.",
+      "Remove from oven. Add the lettuce and serve."
+    ]
+  },
+  {
+    name: "Fish Tacos",
+    ingredients: [
+      { name: "Whitefish", amount: 1, measurement: "l lb" },
+      { name: "Cheese", amount: 1, measurement: "cup" },
+      { name: "Iceberg Lettuce", amount: 2, measurement: "cups" },
+      { name: "Tomatoes", amount: 2, measurement: "large" },
+      { name: "Tortillas", amount: 3, measurement: "med" }
+    ],
+    steps: [
+      "Cook the fish on the grill until cooked through.",
+      "Place the fish on the 3 tortillas.",
+      "Top them with lettuce, tomatoes, and cheese."
+    ]
+  }
+];
+
+function Recipe({ name, ingredients, steps }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section id={name.toLowerCase().replace(/ /g, "-")}>
+      <h1>{name}</h1>
+      <ul className="ingredients">
+        {ingredients.map((ingredient, i) => (
+          <li key={i}>{ingredient.name}</li>
+        ))}
+      </ul>
+      <section className="instructions">
+        <h2>Cooking Instructions</h2>
+        {steps.map((step, i) => (
+          <p key={i}>{i}: {step}</p>
+        ))}
+      </section>
+    </section>
   );
 }
 
-export default App;
+
+function Menu({title, recipes}) {
+  return (
+    <article>
+      <header>
+        <h1>{title}</h1>
+      </header>
+      <div className="recipes">
+        {recipes.map((recipe, i) => (
+          <Recipe key={i} {...recipe} />
+        ))}
+      </div>
+    </article>
+  );
+}
+
+
+export {Menu, data};
