50330103fcf77e58c344ed68b36513542ad9cb97
Erik Rosness <erik.rosness@tandbergradio.com>
2019-10-16 15:10:32 +0200

    Add rate to recipe

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {Instructions} from './Instructions';
 import {Ingredients} from './Ingredients';
+import {Rate} from './Rate';
 
 function Recipe({ name, ingredients, steps }) {
   return (
     <section id={name.toLowerCase().replace(/ /g, "-")}>
-      <h1>{name}</h1>
+      <h3>{name}</h3>
+      <Rate totalStars = {6} />
       <Ingredients ingredients={ingredients} />
       <Instructions title="Goodie foodie!" steps={steps} />
     </section>
