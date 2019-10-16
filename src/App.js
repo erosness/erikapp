512f341effd03a6e5a9b1508b8f3cb105569ff3b
Erik Rosness <erik.rosness@tandbergradio.com>
2019-10-16 09:16:07 +0200

    Some cleanup.

--- src/App.js
+++ src/App.js
@@ -4,28 +4,6 @@ import './App.css';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-function callout() {
-  const string = "Restaurants in Hanalei in operation";
-  const urlFriendly = string => string.split(" ");
-  const reversex = array => array.reverse();
-  const connect = array => {
-    if (array.length === 1) {
-      return array[0];
-    } else {
-      return (array[0] + "-" + connect(array.slice(1)));
-    }
-  }
-
-
-  console.log(connect(reversex(reversex(urlFriendly(string))))); // "Restaurants-in-Hanalei"
-  console.log(string);
-
-}
-
-const headerLine = React.createElement("h1", { id: "recipe-0" }, "Baked Salmon");
-console.log(headerLine);
-
-
 function Cat({ name }) {
   return (
     <React.Fragment>
